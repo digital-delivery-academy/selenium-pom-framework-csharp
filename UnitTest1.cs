ea22f21973d328b1c5f83aa8b634fe693e83d913
Steve Walton <steve.walton@evoco.co.uk>
2021-03-24 21:10:42 +0000

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace selenium_pom_framework_csharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
